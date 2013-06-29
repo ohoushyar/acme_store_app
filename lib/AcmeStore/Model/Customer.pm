3bcc55b8d8973b5892f4b817e9d9674e6a1a6440
Omid Houshyar <ohoushyar@gmail.com>
2013-06-30 04:16:59 +0800

    Added save method

--- lib/AcmeStore/Model/Customer.pm
+++ lib/AcmeStore/Model/Customer.pm
@@ -3,21 +3,35 @@ package AcmeStore::Model::Customer;
 use strict;
 use warnings;
 
-use Mo qw(build default required);
+use Mo qw(default required);
 extends 'AcmeStore::Model::Base';
 
+has 'id' => (is => 'rw', required => 1);
 has 'first_name' => (is => 'rw');
 has 'last_name' => (is => 'rw');
 
-sub BUILD {
-    my $self = shift;
-
-
-}
-
 sub save {
     my $self = shift;
+    my $schema = $self->schema;
+    my $result;
 
+    #$schema->storage->debug(1);
+    eval {
+        $result = $schema->resultset('Customer')->find_or_create(
+            {
+                id          => $self->id,
+                first_name  => $self->first_name,
+                last_name   => $self->last_name,
+            },
+            { key => 'primary' },
+        );
+    };
+    if ($@) {
+        Carp::croak("Unable to save customer; ERROR: [$@]");
+        return;
+    }
+
+    return $result;
 }
 
 1;
